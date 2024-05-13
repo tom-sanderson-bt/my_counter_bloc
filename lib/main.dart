15dbdf71aeae099c7729a5c7b3a9f0dd0aee4296
Tom Sanderson <thomas.sanderson@bt.com>
2024-05-13 21:48:04 +0100

    add counter bloc code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_counter_bloc/blocs/counter/counter_bloc.dart';
+import 'package:my_counter_bloc/other_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyCounter Bloc',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'MyCounter Bloc',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -26,28 +32,50 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text(
-          '0',
-          style: TextStyle(fontSize: 52.0),
+    return BlocListener<CounterBloc, CounterState>(
+      listener: (context, state) {
+        if (state.counter == 3) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    content: Text('Counter is ${state.counter}'));
+              });
+        }
+
+        if (state.counter < 0) {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return OtherPage();
+          }));
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Text(
+            '${context.watch<CounterBloc>().state.counter}',
+            style: TextStyle(fontSize: 52.0),
+          ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {},
-            heroTag: 'increment',
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(width: 10.0),
-          FloatingActionButton(
-            onPressed: () {},
-            heroTag: 'decrement',
-            child: const Icon(Icons.add),
-          ),
-        ],
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                context.read<CounterBloc>().add(IncrementCounterEvent());
+              },
+              heroTag: 'increment',
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(width: 10.0),
+            FloatingActionButton(
+              onPressed: () {
+                context.read<CounterBloc>().add(DecrementCounterEvent());
+              },
+              heroTag: 'decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
       ),
     );
   }
